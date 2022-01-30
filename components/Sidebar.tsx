87a0e3e06d6908d66c7c100b0ec95e07acec31e2
gomesIgor21 <igorb.21gomes@gmail.com>
2022-01-30 18:10:45 -0300

    Adjusments for Sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -29,7 +29,8 @@ function Sidebar() {
 	return (
 		<div
 			className="text-gray-500 p-5 text-xs lg:text-sm border-r border-gray-900 
-			overflow-y-scroll scrollbar-hide h-screen sm:max-w-[12rem] lg:max-w-[15rem] hidden md:inline-flex flex-col"
+			overflow-y-scroll scrollbar-hide h-screen sm:max-w-[12rem] lg:max-w-[15rem] 
+			hidden md:inline-flex flex-col pb-36"
 		>
 			<div className="space-y-4">
 				<button className="flex items-center space-x-2 hover:text-white">
