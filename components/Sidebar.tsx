c71692f8285a694e389ccd09b8d7885bef2f9e41
gomesIgor21 <igorb.21gomes@gmail.com>
2022-01-30 16:26:24 -0300

    Render Playlist names

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -7,19 +7,31 @@ import {
 	RssIcon,
 } from '@heroicons/react/outline';
 import { signOut, useSession } from 'next-auth/react';
+import { useEffect, useState } from 'react';
+import { useRecoilState } from 'recoil';
+import { playlistIdState } from '../atoms/playlistAtom';
+import useSpotify from '../hooks/useSpotify';
 
 function Sidebar() {
 	const { data: session, status } = useSession();
-	console.log(session);
+	const [playlists, setPlaylists] = useState([]);
+	const [playlistId, setPlaylistId] = useRecoilState(playlistIdState);
+	const spotifyApi = useSpotify();
+
+	useEffect(() => {
+		if (spotifyApi.getAccessToken()) {
+			spotifyApi.getUserPlaylists().then((data) => {
+				setPlaylists(data.body.items);
+			});
+		}
+	}, [session, spotifyApi]);
+
 	return (
-		<div className="text-gray-500 p-5 text-sm border-r border-gray-900 overflow-y-scroll scrollbar-hide h-screen">
+		<div
+			className="text-gray-500 p-5 text-xs lg:text-sm border-r border-gray-900 
+			overflow-y-scroll scrollbar-hide h-screen sm:max-w-[12rem] lg:max-w-[15rem] hidden md:inline-flex flex-col"
+		>
 			<div className="space-y-4">
-				<button
-					className="flex items-center space-x-2 hover:text-white"
-					onClick={() => signOut()}
-				>
-					Log Out
-				</button>
 				<button className="flex items-center space-x-2 hover:text-white">
 					<HomeIcon className="h-5 w-5" />
 					<p>Home</p>
@@ -50,7 +62,17 @@ function Sidebar() {
 			</div>
 
 			{/* Playlist */}
-			<p className="cursor-pointer hover:text-white">Playlist name</p>
+			{playlists?.map((playlist) => (
+				<p
+					key={playlist.id}
+					onClick={() => setPlaylistId(playlist.id)}
+					className="cursor-pointer pt-1 hover:text-white"
+				>
+					{playlist.name.length <= 20
+						? playlist.name
+						: playlist.name.substring(0, 20) + '...'}
+				</p>
+			))}
 		</div>
 	);
 }
