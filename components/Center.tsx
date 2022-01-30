27c609a4a9b1c74d26c587c37dbcb26ede4aeac6
gomesIgor21 <igorb.21gomes@gmail.com>
2022-01-30 16:27:12 -0300

    Rendering Playlist tracks

--- components/Center.tsx
+++ components/Center.tsx
@@ -1,7 +1,11 @@
 import { ChevronDownIcon } from '@heroicons/react/outline';
-import { useSession } from 'next-auth/react';
+import { signOut, useSession } from 'next-auth/react';
 import { useEffect, useState } from 'react';
 import { shuffle } from 'loadsh';
+import { useRecoilState, useRecoilValue } from 'recoil';
+import { playlistIdState, playlistState } from '../atoms/playlistAtom';
+import useSpotify from '../hooks/useSpotify';
+import Songs from './Songs';
 
 const colors = [
 	'from-indigo-500',
@@ -14,19 +18,31 @@ const colors = [
 ];
 
 function Center() {
+  const spotifyApi = useSpotify();
 	const { data: session } = useSession();
 	const [color, setColor] = useState(null);
+  const playlistId = useRecoilValue(playlistIdState);
+  const [playlist, setPlaylist] = useRecoilState(playlistState);
 
 	useEffect(() => {
     setColor(shuffle(colors).pop());
-  }, []);
+  }, [playlistId]);
+
+  useEffect(() => {
+    spotifyApi.getPlaylist(playlistId).then((data) => {
+			setPlaylist(data.body)
+		}).catch((err) => {
+			console.log("Something went wrong!", err);
+		});
+  }, [spotifyApi, playlistId])
 
 	return (
-		<div className="flex-grow text-white">
+		<div className="flex-grow text-white h-screen overflow-y-scroll scrollbar-hide">
 			<header className="absolute top-5 right-8">
 				<div
 					className="flex items-center bg-black space-x-3 opacity-90 
           hover:opacity-80 cursor-pointer rounded-full p-1 pr-2"
+					onClick={signOut}
 				>
 					<img className="rounded-full w-8 h-8" src={session?.user.image} />
 					<h2>{session?.user.name}</h2>
@@ -38,8 +54,20 @@ function Center() {
 				className={`flex items-end space-x-7 bg-gradient-to-b
         to-black ${color} h-80 text-white p-8`}
 			>
-				{/* <img src="" alt="" /> */}
+				<img className='h-44 w-44 shadow-2xl' src={playlist?.images?.[0]?.url} />
+				<div>
+					<p>PLAYLIST</p>
+					
+					<h1 className='text-2xl md:text-3xl lg:text-5xl'>
+						{playlist?.name.length < 35 ? playlist?.name : playlist?.name.substring(0,35) + '...'}
+					</h1> 
+					
+				</div>
 			</section>
+
+			<div>
+				<Songs />
+			</div>
 		</div>
 	);
 }
