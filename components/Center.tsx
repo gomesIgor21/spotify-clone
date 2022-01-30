aefc0a2de4e322029338ffa21d452056ae0c9a56
gomesIgor21 <igorb.21gomes@gmail.com>
2022-01-30 11:25:20 -0300

    Created Header

--- components/Center.tsx
+++ components/Center.tsx
@@ -1,9 +1,47 @@
+import { ChevronDownIcon } from '@heroicons/react/outline';
+import { useSession } from 'next-auth/react';
+import { useEffect, useState } from 'react';
+import { shuffle } from 'loadsh';
+
+const colors = [
+	'from-indigo-500',
+	'from-blue-500',
+	'from-green-500',
+	'from-red-500',
+	'from-yellow-500',
+	'from-pink-500',
+	'from-purple-500',
+];
+
 function Center() {
-  return (
-    <div className="flex flex-grow text-white">
-      
-    </div>
-  )
+	const { data: session } = useSession();
+	const [color, setColor] = useState(null);
+
+	useEffect(() => {
+    setColor(shuffle(colors).pop());
+  }, []);
+
+	return (
+		<div className="flex-grow text-white">
+			<header className="absolute top-5 right-8">
+				<div
+					className="flex items-center bg-black space-x-3 opacity-90 
+          hover:opacity-80 cursor-pointer rounded-full p-1 pr-2"
+				>
+					<img className="rounded-full w-8 h-8" src={session?.user.image} />
+					<h2>{session?.user.name}</h2>
+					<ChevronDownIcon className="h-4 w-4" />
+				</div>
+			</header>
+
+			<section
+				className={`flex items-end space-x-7 bg-gradient-to-b
+        to-black ${color} h-80 text-white p-8`}
+			>
+				{/* <img src="" alt="" /> */}
+			</section>
+		</div>
+	);
 }
 
-export default Center
+export default Center;
